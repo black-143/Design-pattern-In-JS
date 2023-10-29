004fecf01bb74acf4f258801567f3941518b7dc1
Pravallika Yakkala <59355045+black-143@users.noreply.github.com>
2023-10-29 20:14:04 +0530

    Add files via upload

---
+++ asynctasksinsequence.js
@@ -0,0 +1,59 @@
+function createAsyncTask(){
+	const value=Math.floor(Math.random()*10)
+
+
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(value < 5){
+				reject(`Error ${value}`)
+			}else{
+				resolve(value*1000)
+			}
+		})
+	})
+}
+
+let tasks=[
+
+	createAsyncTask(),
+	createAsyncTask(),
+	createAsyncTask(),
+	createAsyncTask(),
+	createAsyncTask()
+]
+
+const asyncSequence=async (tasks,callback)=>{
+	let results=[]
+	let errors=[]
+	let completed=0;
+
+	// tasks.reduce((prev,curr)=>{
+	// 	return prev.finally(()=>{
+	// 		return curr.then((val)=>{
+	// 			results.push(val)
+	// 		}).catch((err)=>{errors.push(err)}).finally(()=>{
+	// 			completed++
+	// 			if(completed == tasks.length){
+	// 				callback(errors,results)
+	// 			}
+	// 		})
+	// 	})
+	// },Promise.resolve());
+
+	for (let task of tasks){
+		try{
+			let res= await task;
+			results.push(res)
+
+		}catch(e){
+			errors.push(e)
+		}
+	}
+
+	callback(errors,results)
+
+}
+asyncSequence(tasks,(error,result)=>{
+	console.log(error)
+	console.log(result)
+})
